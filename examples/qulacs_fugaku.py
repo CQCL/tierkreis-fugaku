4a8cd8e6318e14c1daf9c7f215802e79a093c54a
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-16 15:13:17 +0100

    Make the number of threads configurable

--- examples/qulacs_fugaku.py
+++ examples/qulacs_fugaku.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from sys import argv
 from typing import Literal
@@ -21,7 +22,7 @@ uv = UvExecutor(WORKERS_DIR, storage.logs_path)
 command = (
     ". /vol0004/apps/oss/spack/share/spack/setup-env.sh && "
     "spack load /slvpnrm && "  # load boost@1.83.0%fj@4.10.0 arch=linux-rhel8-a64fx
-    "env OMP_NUM_THREADS=10 UV_PROJECT_ENVIRONMENT=compute_venv uv run main.py"
+    f"env OMP_NUM_THREADS={os.environ.get("OMP_NUM_THREADS", 10)} UV_PROJECT_ENVIRONMENT=compute_venv uv run main.py"
 )
 
 
