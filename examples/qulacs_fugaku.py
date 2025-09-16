97a8f48d7407f2779f0da967f12096d0ab79e2ee
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-16 08:46:25 +0100

    Try env var pass through

--- examples/qulacs_fugaku.py
+++ examples/qulacs_fugaku.py
@@ -49,7 +49,7 @@ def pjsub_uv_executor(group_name: str, logs_path: Path) -> PJSUBExecutor:
         error_path=Path(logs_path),
         include_no_check_directory_flag=True,
     )
-    return PJSUBExecutor(WORKERS_DIR, logs_path, spec)
+    return PJSUBExecutor(WORKERS_DIR, logs_path, spec, command=" pjsub -X ")
 
 
 def main(group_name: str):
