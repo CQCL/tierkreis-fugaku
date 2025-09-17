591f51fd29de89acaf9a6676382f5f667dadbbd9
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-17 14:38:57 +0100

    qulacs only example

--- workers/tkr_qulacs/scripts/simulate.py
+++ workers/tkr_qulacs/scripts/simulate.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from time import time
 
@@ -5,10 +6,15 @@ from pytket.qasm.qasm import circuit_from_qasm
 from pytket.extensions.qulacs.backends.qulacs_backend import QulacsBackend
 
 circuit_file = Path(__file__).parent.parent / "compiled_circuit.qasm"
-circuit = circuit_from_qasm(circuit_file, maxwidth=100)
+circuit = circuit_from_qasm(circuit_file, maxwidth=1000)
 start = time()
 print(f"Started at {start}")
-compiled_circuit = QulacsBackend().run_circuits([circuit], 1)
+n_shots = 1000
+print(n_shots)
+
+print(os.environ.get("OMP_NUM_THREADS"))
+results = QulacsBackend().run_circuits([circuit], n_shots, False)
+print(results)
 end = time()
 
 print(f"Time taken: {end-start}")
