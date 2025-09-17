db1c12f7053a601ec8058fba4373c7b973a832e6
Matthew Burke <matthew.burke@quantinuum.com>
2025-09-17 10:45:09 +0100

    Control run

--- examples/qulacs_fugaku.py
+++ examples/qulacs_fugaku.py
@@ -22,7 +22,8 @@ uv = UvExecutor(WORKERS_DIR, storage.logs_path)
 command = (
     ". /vol0004/apps/oss/spack/share/spack/setup-env.sh && "
     "spack load /slvpnrm && "  # load boost@1.83.0%fj@4.10.0 arch=linux-rhel8-a64fx
-    "OMP_NUM_THREADS=12 UV_PROJECT_ENVIRONMENT=compute_venv mpiexec -np ${PJM_MPI_PROC} uv run main.py"
+    # "OMP_NUM_THREADS=12 UV_PROJECT_ENVIRONMENT=compute_venv mpiexec -np ${PJM_MPI_PROC} uv run main.py"
+    "UV_PROJECT_ENVIRONMENT=compute_venv uv run main.py"
 )
 
 
